30590a9d7afeecf7f62f766992a12225b1ce5646
xutao <514335188@qq.com>
2021-11-06 23:32:04 +0800

    Update Cache Manager.

--- common/src/main/java/coo/lxt/island/common/dao/AbstractManager.java
+++ common/src/main/java/coo/lxt/island/common/dao/AbstractManager.java
@@ -1,5 +1,9 @@
 package coo.lxt.island.common.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
 import org.redisson.api.RLock;
 import org.redisson.api.RMap;
@@ -8,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -20,12 +21,20 @@ import java.util.stream.Collectors;
 /**
  * This manager provide redis loading cache for DB interaction.
  */
+@Slf4j
 public abstract class AbstractManager {
 
-    @Value("${common.dao.lock.prefix:lock.}")
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Nullable<T> {
+        private T value;
+    }
+
+    @Value("${common.dao.cache.lock.prefix:lock.}")
     private String LOCK_PREFIX;
 
-    @Value("${common.dao.lock.timeout:3}")
+    @Value("${common.dao.cache.lock.timeout:3}")
     private int LOCK_TIMEOUT;
 
     @Value("${common.dao.cache.timeout:30}")
@@ -41,19 +50,26 @@ public abstract class AbstractManager {
      * Set a very long timeout (1 year) for redis key to enable eviction policy volatile-lru.
      */
     protected <T> Optional<T> read(String key, Supplier<Optional<T>> dbReader) {
-        RBucket<T> bucket = redissonClient.getBucket(key);
-        if (bucket.isExists()) {
-            return Optional.ofNullable(bucket.get());
+        RBucket<Nullable<T>> bucket = redissonClient.getBucket(key);
+        Optional<Nullable<T>> value = Optional.ofNullable(bucket.get());
+        if (value.isPresent()) {
+            return value.map(Nullable::getValue);
         } else {
+            log.warn("Miss in cache for key: {}", key);
             RLock lock = redissonClient.getLock(LOCK_PREFIX + key);
             try {
-                lock.lock(LOCK_TIMEOUT, TimeUnit.SECONDS);
-                Optional<T> value = Optional.ofNullable(bucket.get());
-                if (!value.isPresent()) {
-                    value = dbReader.get();
-                    bucket.set(value.orElse(null), CACHE_TIMEOUT, TimeUnit.MINUTES);
+                boolean locked = lock.tryLock(LOCK_TIMEOUT, LOCK_TIMEOUT, TimeUnit.SECONDS);
+                value = Optional.ofNullable(bucket.get());
+                if (locked && !value.isPresent()) {
+                    Optional<T> res = dbReader.get();
+                    value = Optional.of(new Nullable<>(res.orElse(null)));
+                    bucket.set(value.get(), CACHE_TIMEOUT, TimeUnit.MINUTES);
                 }
-                return value;
+                return value.map(Nullable::getValue);
+            } catch (InterruptedException e) {
+                log.error("Acquiring lock interrupted: {}.", key, e);
+                Thread.currentThread().interrupt();
+                return value.map(Nullable::getValue);
             } finally {
                 lock.unlock();
             }
@@ -61,23 +77,30 @@ public abstract class AbstractManager {
     }
 
     protected <T> Optional<T> read(String key, String field, Supplier<List<T>> dbReader, Function<T, String> fieldMapper) {
-        RMap<String, T> bucket = redissonClient.getMap(key);
-        if (bucket.isExists()) {
-            return Optional.ofNullable(bucket.get(field));
+        RMap<String, Nullable<T>> bucket = redissonClient.getMap(key);
+        Optional<Nullable<T>> value = Optional.ofNullable(bucket.get(field));
+        if (value.isPresent()) {
+            return value.map(Nullable::getValue);
         } else {
+            log.warn("Miss in cache for key: {}, field: {}", key, field);
             RLock lock = redissonClient.getLock(LOCK_PREFIX + key);
             try {
-                lock.lock(LOCK_TIMEOUT, TimeUnit.SECONDS);
-                Map<String, T> map = bucket.readAllMap();
-                if (map.containsKey(field)) {
-                    map = dbReader.get().stream().collect(Collectors.toMap(fieldMapper, (o) -> o));
-                    if (!map.containsKey(field)) {
-                        map.put(field, null);
+                boolean locked = lock.tryLock(LOCK_TIMEOUT, LOCK_TIMEOUT, TimeUnit.SECONDS);
+                value = Optional.ofNullable(bucket.get(field));
+                if (locked && !value.isPresent()) {
+                    Map<String, Nullable<T>> map = dbReader.get().stream().collect(Collectors.toMap(fieldMapper, Nullable::new));
+                    if (locked && !map.containsKey(field)) {
+                        map.put(field, new Nullable<>());
                     }
+                    value = Optional.ofNullable(map.get(field));
                     bucket.putAll(map);
                     bucket.expire(CACHE_TIMEOUT, TimeUnit.MINUTES);
                 }
-                return Optional.ofNullable(map.get(field));
+                return value.map(Nullable::getValue);
+            } catch (InterruptedException e) {
+                log.error("Acquiring lock interrupted: {}.", key, e);
+                Thread.currentThread().interrupt();
+                return value.map(Nullable::getValue);
             } finally {
                 lock.unlock();
             }
@@ -85,20 +108,27 @@ public abstract class AbstractManager {
     }
 
     protected <T> List<T> readAll(String key, Supplier<List<T>> dbReader, Function<T, String> fieldMapper) {
-        RMap<String, T> bucket = redissonClient.getMap(key);
-        if (bucket.isExists()) {
-            return new ArrayList<>(bucket.values());
+        RMap<String, Nullable<T>> bucket = redissonClient.getMap(key);
+        Collection<Nullable<T>> values = bucket.readAllValues();
+        if (!CollectionUtils.isEmpty(values)) {
+            return values.stream().map(Nullable::getValue).filter(Objects::nonNull).collect(Collectors.toList());
         } else {
+            log.warn("Miss in cache for key: {}", key);
             RLock lock = redissonClient.getLock(LOCK_PREFIX + key);
             try {
-                lock.lock(LOCK_TIMEOUT, TimeUnit.SECONDS);
-                List<T> values = new ArrayList<>(bucket.values());
-                if (CollectionUtils.isEmpty(values)) {
-                    values = dbReader.get();
-                    bucket.putAll(values.stream().collect(Collectors.toMap(fieldMapper, (o) -> o)));
+                boolean locked = lock.tryLock(LOCK_TIMEOUT, LOCK_TIMEOUT, TimeUnit.SECONDS);
+                values = bucket.readAllValues();
+                if (locked && CollectionUtils.isEmpty(values)) {
+                    Map<String, Nullable<T>> map = dbReader.get().stream().collect(Collectors.toMap(fieldMapper, Nullable::new));
+                    values = map.values();
+                    bucket.putAll(map);
                     bucket.expire(CACHE_TIMEOUT, TimeUnit.MINUTES);
                 }
-                return values;
+                return values.stream().map(Nullable::getValue).filter(Objects::nonNull).collect(Collectors.toList());
+            } catch (InterruptedException e) {
+                log.error("Acquiring lock interrupted: {}.", key, e);
+                Thread.currentThread().interrupt();
+                return values.stream().map(Nullable::getValue).filter(Objects::nonNull).collect(Collectors.toList());
             } finally {
                 lock.unlock();
             }
